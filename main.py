71041bc8df97869030354f1e01b9345f34cb11c1
SystemPC <prakharchaturvedi20287@gmail.com>
2026-04-26 20:22:57 +0530

    numpy day2

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import numpy as np
 # print(np.__version__)
 
 a = np.array([2,4,5])
-# print(a)
-# print(a.shape)
-# print(a.dtype)
-# print(a.ndim)
-# print(a.size)
-# print(a.itemsize)
+print(a)
+print(a.shape)
+print(a.dtype)
+print(a.ndim)
+print(a.size)
+print(a.itemsize)
 
 # day 2
 print(a[0])
