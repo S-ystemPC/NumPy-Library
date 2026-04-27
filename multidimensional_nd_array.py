94bb5412719f3533ca9621c182d06ac11abff0d8
SystemPC <prakharchaturvedi20287@gmail.com>
2026-04-27 17:45:02 +0530

    multidimensional_nd_array.py

---
+++ multidimensional_nd_array.py
@@ -0,0 +1,21 @@
+import numpy as np
+a1 = np.array([[1,2], [3,4]])
+
+print(a1)
+print(a1.shape)
+
+
+a2 = np.array([[1,2,6], [3,4,8]])
+print(a2)
+print(a2.shape)
+
+print(a2[0])
+print(a2[0][0])
+
+
+a3 = np.array([[1,2,6], [3,4,8]])
+# To print all rows an 0'th column
+print(a3[:,0])
+
+# To print 0'th row and all columns
+print(a3[0,:])
